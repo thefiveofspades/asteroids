6ba9786dd1766a47fa86e49fd887930726aee32a
thefiveofspades <alex.j.cameron@outlook.com>
2025-05-31 00:22:17 +1000

    implement asteroids

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, circleshape):
+        if self.radius + circleshape.radius >= self.position.distance_to(circleshape.position):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
+    
     clock = pygame.time.Clock()
     dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(x, y)
+    asteroidfield = AsteroidField()
+
 
     while True:
         for event in pygame.event.get():
@@ -17,8 +32,14 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
